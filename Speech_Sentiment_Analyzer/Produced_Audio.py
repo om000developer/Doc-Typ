1c758e6dee1607701d9a4ba40963a7ba7af3e28d
Om Agarwal <om000developer@gmail.com>
2020-05-03 02:54:45 -0400

    Add files via upload

---
+++ Speech_Sentiment_Analyzer/Produced_Audio.py
@@ -0,0 +1,26 @@
+import librosa
+import soundfile
+import pickle
+import numpy as np
+
+loaded_model = pickle.load(open("Model.sav", 'rb'))
+
+def extract_feature(file_name, mfcc, chroma, mel):
+    with soundfile.SoundFile(file_name) as sound_file:
+        X = sound_file.read(dtype="float32")
+        sample_rate=sound_file.samplerate
+        if chroma: stft=np.abs(librosa.stft(X))
+        result=np.array([])
+        if mfcc:
+            mfccs=np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            result=np.hstack((result, mfccs))
+        if chroma:
+            chroma=np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+            result=np.hstack((result, chroma))
+        if mel:
+            mel=np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+            result=np.hstack((result, mel))
+    return result
+
+audio=[extract_feature("Test_Audio.wav", mfcc=True, chroma=True, mel=True)]
+prediction=str(loaded_model.predict(audio)[0])
